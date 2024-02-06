a6d0040ea8ab047fce0f3bd952eb12dc142c95d2
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-06 14:18:13 -0400

    Update my files

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,2 +1,7 @@
 print('Hola Mundo!')
-print('John Doe')
+mensaje = 'John Doe' # String value
+gpa = 3.14 # number (real) value
+isValid = True # boolean value
+print(mensaje)
+print(gpa)
+print(isValid)
