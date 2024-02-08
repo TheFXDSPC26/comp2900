9e3b1675a90ccf0893c9f7518c551c9f9ab3ecd2
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-08 18:03:34 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -3,16 +3,18 @@ num2 = int(input("Enter second number: "))
 
 operator = input("Enter operator sign to choose math operation: ")
 
+while (operator != 'e'):
 
-if (operator == '+'):
-    print(f'Sum is {num1+num2}')
-elif (operator == '-'):
-    print(f'Difference is {num1-num2}')
-elif (operator == '*'):
-    print(f'Product is {num1*num2}')
-elif (operator == '/'):
-    print(f'Division is {num1/num2}')
-else :
-    print('Invalid operator')
+    if (operator == '+'):
+        print(f'Sum is {num1+num2}')
+    elif (operator == '-'):
+        print(f'Difference is {num1-num2}')
+    elif (operator == '*'):
+        print(f'Product is {num1*num2}')
+    elif (operator == '/'):
+        print(f'Division is {num1/num2}')
+    else :
+        print('Invalid operator')
+        operator = input("Enter operator sign to choose math operation: ")
 
     
