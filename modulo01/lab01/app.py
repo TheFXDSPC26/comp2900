e4716dd043d17c232875312b5ee5cd9fdd8ad576
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-08 13:54:37 -0400

    Update file

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,12 +1,8 @@
-#print('Hola Mundo!')
-#mensaje = 'John Doe' # String value
-#gpa = 3.14 # number (real) value
-#isValid = True # boolean value
-#print(mensaje)
-#print(gpa)
-#print(isValid)
-#print(f'{mensaje} tu GPA es {gpa:3.4}')
-patient = "John Smith"
-age = 20
-new_patient = True
-print(f'{patient} is {age} years and it is {new_patient} that he is new')
+print('Hola Mundo!')
+mensaje = 'John Doe' # String value
+gpa = 3.14 # number (real) value
+isValid = True # boolean value
+print(mensaje)
+print(gpa)
+print(isValid)
+print(f'{mensaje} tu GPA es {gpa:3.4}')
