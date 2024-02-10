1ab4317ed8ae29278c540a7d2bedd4761dff4626
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-10 18:59:07 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -1,18 +1,7 @@
-enterString = input('S to go to settings, E to exit, V to show program version, A to show about information: ')
+age = 24
+weight = 178.5
+namePerson1 = 'Felipe Diaz'
+isMale = True
 
-while (enterString !='E'):
-
-    if (enterString == 'S' or enterString == 's'):
-            print(f'No settings to change')
-            enterString = input('S to go to settings, E to exit, V to show program version, A to show about information: ')
-    elif (enterString == 'E' or enterString == 'e'):
-            print(f'Exiting program')   
-    elif (enterString == 'V' or enterString == 'v'):
-            print(f'Program version is 1.29.3')
-            enterString = input('S to go to settings, E to exit, V to show program version, A to show about information: ')
-    elif (enterString == 'A'):
-            print(f'Sample program version 1.29.3 \nCopyright 2024 by Elliot Productions. \nAll rights reserved.')
-            enterString = input('S to go to settings, E to exit, V to show program version, A to show about information: ')
-    else:
-            print('Invalid option selected')
-            enterString = input('S to go to settings, E to exit, V to show program version, A to show about information: ')
+print(f'My name is {namePerson1}.\nI am {age} years old, weight approximately {weight} pounds, and it is {isMale} that I am a man.')
+   
