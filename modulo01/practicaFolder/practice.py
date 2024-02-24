034317f063f96d945f7511f57226045869ec5d43
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-24 13:46:31 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -1,10 +1,9 @@
 def saludos(mensaje="Mundo"):
     print(f'Hola {mensaje}')
 
-def saludo(msg):
-    print(f'Hola {msg}')
-   
+def saludo(msg, edad):
+    print(f'Hola {msg}, tu tienes {edad}')  
 
-saludo('Felipe')
-saludo('Kevin')
-saludo('Luis')
+saludo('Felipe', 25)
+saludo('Kevin', 17)
+saludo('Luis', 56)
