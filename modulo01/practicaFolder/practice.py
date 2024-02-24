f6e450e7426233c65cfddd82d84f19dcded59f31
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-24 13:14:17 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -1,12 +1,10 @@
 def saludos(mensaje="Mundo"):
     print(f'Hola {mensaje}')
 
+def saludo(msg):
+    print(f'Hola {msg}')
+   
 
-
-def saludo(msg='Felipe', msg2='Puerto Rico'):
-    print(f'Hola {msg}, Tu eres de {msg2}')
-
-def saludoz(msg='',msg1='l',msg2='g'):
-    print(f'Hola')
-
-saludo('Fern','Liz')
+saludo('Felipe')
+saludo('Kevin')
+saludo('Luis')
