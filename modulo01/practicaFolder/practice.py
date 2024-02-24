fb1ee187cf27259e7ee0edf37e80ea903fef0934
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-24 16:45:31 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -1,8 +1,3 @@
-def create_name(first, last):
-    first = first.capitalize()
-    last = last.capitalize()
-    return first + " " + last
-
-full_name = create_name("felipe","diaz")
-
-print(full_name)
+c = "python is great"
+print(c.find('a'))
+print(c)
