41856c5d56fdcea85273386fe197bbac63c879e8
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-25 00:21:26 -0400

    Update file

--- modulo01/practicaFolder/practice.py
+++ modulo01/practicaFolder/practice.py
@@ -1,14 +1,6 @@
-enterWeight = float(input('Enter weight (in pounds): '))
+i = 0
 
-
-weight = enterWeight
-
-enterUnit = input('Would you like the information shown in (k)kg or (l)lb: ')
-
-if (enterUnit  == 'l'):
-    print(f'{weight:.2f}')
-elif (enterUnit == 'k'):
-    kg= weight * 0.453592
-    print(f'{kg:.2f}')
-else:
-    print('Error')
+while not i>5:
+  
+    print(i)
+    i+=1
