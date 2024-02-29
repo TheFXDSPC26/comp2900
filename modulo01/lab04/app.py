ab3370929d43281fe2033cdfafe90c3efb891221
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-29 13:46:10 -0400

    Update file

--- modulo01/lab04/app.py
+++
@@ -1,8 +0,0 @@
-def suma(a=5,b=1):
-    return(a+b)
-
-result= suma()
-print(result)
-
-result= suma(5,10)
-print(result)
--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,2 +1,13 @@
-guessNum = int(input('Welcome to the number guessing game!\nThe correct number is between 1 to 10\n\nEnter your number to get started: '))
-
+tries = 0
+while tries <3:
+    print(f'You have {3-tries} attempt(s) left')
+    guessNum = int(input('Welcome to the number guessing game!\n\nThe correct number is between 1 to 10\n\nEnter your number to get started: '))
+    if guessNum == 9:
+        print('Correct answer entered!!')
+        exit()
+    else:
+        tries+=1
+        if tries == 2:
+            print('You lose')
+        else:
+            print('\nTry again')        
