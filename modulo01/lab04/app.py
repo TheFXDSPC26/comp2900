ea96bb2a84d976eded955b5f5a2c980138b6cf66
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-29 14:24:35 -0400

    Update file

---
+++ modulo01/lab04/app.py
@@ -0,0 +1,13 @@
+import array
+
+arr = array.array('i')
+
+for i in range(0,4):
+    inNum = int(input('Enter number: '))
+    arr.append(inNum)
+    
+for i in range(4):
+        print(f'Indice {i} - Valor {arr[i]}')
+
+for n in arr:
+      print(f'Valor {n}')
