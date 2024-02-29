d17c059f0f99c671a2a23f4253e7157579745580
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-02-29 14:00:47 -0400

    Update file

--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,9 +1,13 @@
 tries = 0
 while tries <3:
-    print(f'You have {3-tries} attempt(s) left')
+    if tries ==2:
+        print(f'You have {3-tries} attempt left')
+    else:
+        print(f'You have {3-tries} attempts left')
+    
     guessNum = int(input('Welcome to the number guessing game!\n\nThe correct number is between 1 to 10\n\nEnter your number to get started: '))
     if guessNum == 9:
-        print('Correct answer entered!!')
+        print('Correct answer entered!!\n\nYou Win!!')
         exit()
     else:
         tries+=1
