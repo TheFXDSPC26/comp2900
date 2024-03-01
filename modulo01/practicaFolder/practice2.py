d933ca48960b3846b5cb3b54a509ad015ee401c6
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-03-01 01:22:54 -0400

    Update file

--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,5 +1,39 @@
-enterCommand = input('Welcome to the Car Game! Type help for a list of commands to get started: ')
-
-if enterCommand == 'help':
-    commandProgram = ('Start - start car\n\nStop - stop car\n\nQuit - quit program')
-    
+print('Welcome to the Car Game! Type help for a list of commands to get started: ')
+i = 0
+while i<999:
+    z=1
+    y=2
+    c=0    
+    enterCommand = input('Type choice\n\n(Input) to type or (help) for list of commands: ')
+    if enterCommand == 'help':      
+            commandProgram = input('Start - start car\n\nStop - stop car\n\nQuit - quit program: ')
+            commandProgram = commandProgram.upper()
+            if commandProgram == 'START':
+                    i = z
+            elif commandProgram == 'STOP':
+                    i = y
+            else:
+                    i = c
+            if i == 1:
+                    print('Car started!')
+            elif i == 2:
+                    print('Car stopped!')
+            else:
+                print('Closing program')
+                exit()
+    else:
+            commandProgram = input('> ')
+            commandProgram = commandProgram.upper()
+            if commandProgram == 'START':
+                    i = z
+            elif commandProgram == 'STOP':
+                    i = y
+            else:
+                    i = c
+            if i == 1:
+                    print('Car started!')
+            elif i == 2:
+                    print('Car stopped!')
+            else:
+                print('Closing program')
+                exit()
