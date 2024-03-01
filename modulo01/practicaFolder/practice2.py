59c0c7a87c21f408ac290d456c93a5914b990469
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-03-01 00:43:01 -0400

    Update file

--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,4 +1,4 @@
 enterCommand = input('Welcome to the Car Game! Type help for a list of commands to get started: ')
 
 if enterCommand == 'help':
-    
+    print('Start - start car\n\nStop - stop car\n\nQuit - quit program')
