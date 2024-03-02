96df55f9314d58bd4f7780986f2924a9b6abe7c1
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-03-02 13:59:38 -0400

    Update file

--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,25 +1,7 @@
-enterCommand = ''
-started = ""
-while True:
-                enterCommand = input('\n\nType command: ').lower()
-                if enterCommand == 'help':
-                        enterCommand = print('\n\nStart - start car\n\nStop - stop car\n\nQuit - quit program\n\n')
-                elif enterCommand == 'start':
-                        if started != True:
-                                started = True
-                                print('Car started!')
-                        elif started == True:
-                                print('Car has been already started!')
-                elif enterCommand == 'stop':
-                        if started == True:
-                                started = False
-                                print('Car stopped!')
-                        elif started == False:
-                                print('Car has been already stopped!')
-                        else:
-                                print('Car has not been started')
-                elif enterCommand == 'quit':
-                        print('Closing program')
-                        break
-                else:
-                        print("I don't undestand that")              
+import array
+
+arr = array.array(1,2,3)
+print(arr)   
+for item in 'Python':
+    print(item)  
+
