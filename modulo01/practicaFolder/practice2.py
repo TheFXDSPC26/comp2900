f5a376c5454a026a67416f308509dc7ec4ed4afd
Felipe X Diaz <fdiaz1927@arecibointer.edu>
2024-03-02 17:46:40 -0400

    Update file

--- modulo01/practicaFolder/practice2.py
+++ modulo01/practicaFolder/practice2.py
@@ -1,10 +1,15 @@
 import array
 
 arr = array.array('d')
- 
-for item in 'Python':
-    print(item)
 
-for i in arr:
-     numVal = input('Type any number: ')
-     arr[i] = arr.append(numVal)
+x = int(input('How many numbers would you like to store in the main array?: '))
+
+for i in range(x):
+     numVal = float(input('Type any number: '))
+     arr.append(numVal)
+
+for y in range(x):
+       print(f'Position {y} = {arr[y]}') 
+
+for i in range(len(arr)):
+      print(arr[i])
